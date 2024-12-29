86aea711bb29fe087bef0aa910df3d54e4b74d25
Atharva Kumbhar <160429511+atharva5683@users.noreply.github.com>
2024-12-29 16:26:24 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,4 +1,5 @@
 def call(){
-  dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP' # the name OWASP is come from the name you save while downloading dependency-check in jenkins system
+  dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP' 
+  # the name OWASP is come from the name you save while downloading dependency-check in jenkins system
   dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
 }
