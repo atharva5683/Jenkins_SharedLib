19f555a2135b625220b253470067af526e249873
Atharva Kumbhar <160429511+atharva5683@users.noreply.github.com>
2024-12-29 16:48:57 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,13 @@
-def call(String Project, String ImageTag, String dockerhubuser){
-  withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
-      sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
-  }
-  sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
+// Before executing this, add your Docker credentials in Jenkins Credentials 
+// with the ID 'docker'. Ensure it includes the username and password.
+
+def call(String Project, String ImageTag, String dockerhubuser) {
+    // Use Jenkins credentials to securely access DockerHub
+    withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
+        // Log in to DockerHub using the credentials
+        sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
+    }
+    
+    // Push the Docker image to the repository
+    sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
 }
