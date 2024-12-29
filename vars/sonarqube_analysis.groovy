446b558b14fcaf1b07310f496972b2959f132296
Atharva Kumbhar <160429511+atharva5683@users.noreply.github.com>
2024-12-29 16:32:44 +0530

    Update sonarqube_analysis.groovy

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,3 +1,5 @@
+// The value of SonarQubeAPI in the label you configured while setting up 
+// the SonarQube in the Jenkins system.
 def call(String SonarQubeAPI, String Projectname, String ProjectKey){
   withSonarQubeEnv("${SonarQubeAPI}"){
       sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -Dsonar.java.binaries=. -X"
